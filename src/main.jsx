d27fa9603b48a71e833c4ccb006db6392fd0fd37
ajilcg <ajilc50@gmail.com>
2024-09-09 16:53:54 +0530

    commit1

--- src/main.jsx
+++ src/main.jsx
@@ -3,18 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-react({ jsxImportSource: "@emotion/react" });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-    <h1>Test</h1>
     <App />    
     </BrowserRouter>
   </React.StrictMode>,
 )
-export default defineConfig({
-  plugins: [react()],
-});

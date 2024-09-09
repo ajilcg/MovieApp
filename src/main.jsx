ca0de299af5480ffab87f393d0a1350b743a5b3f
ajilcg <ajilc50@gmail.com>
2024-09-09 16:27:20 +0530

    commit1

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import mdx from '@mdx-js/rollup'
 import { BrowserRouter } from 'react-router-dom'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+react({ jsxImportSource: "@emotion/react" });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,9 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>,
 )
 export default defineConfig({
-  plugins: [
-    { enforce: 'pre', ...mdx() },
-    react({ include: /\.(mdx|js|jsx|ts|tsx)$/ }),
-    
-  ],
-})
+  plugins: [react()],
+});
